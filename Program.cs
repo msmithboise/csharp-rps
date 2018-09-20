2fc02d4eafde5aa682d06c5f06887aaddc08a49d
Michael Smith <msmithboise@gmail.com>
2018-09-20 12:00:38 -0600

    new repo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_rps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
